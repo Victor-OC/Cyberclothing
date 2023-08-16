2caf7c802f1144c831d08193c3434d05598bd9b2
Victor-OC <victoroprea49@gmail.com>
2023-08-17 01:42:08 +0300

    Added Functionality to add new Products to .json

--- components/AddProductModal.tsx
+++ components/AddProductModal.tsx
@@ -1,8 +1,54 @@
-import React from "react";
-import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions, MenuItem } from "@mui/material";
+import axios from "axios";
 
-const AddProductModal = ({ variant }: { variant: string }) => {
-  const [open, setOpen] = React.useState(false);
+interface Products {
+  id: number;
+  photo: string;
+  name: string;
+  price: number;
+  status: string;
+  description: string;
+  category: string;
+  vendorId:number;
+}
+
+interface AddProductModalProps {
+  variant: string;
+  addProduct: (newProduct: Products) => void;
+}
+
+const AddProductModal: React.FC<AddProductModalProps> = ({ variant, addProduct }) => {
+  const [open, setOpen] = useState(false);
+  const [lastProductId, setLastProductId] = useState<number | null>(null);
+  const [productData, setProductData] = useState<Products>({
+    id: lastProductId || 0,
+    vendorId: 1,
+    name: "",
+    photo: "",
+    price: 0,
+    status: "in stock",
+    description: "",
+    category: "",
+  });
+
+  useEffect(() => {
+    fetchLastProductId()
+      .then((id) => setLastProductId(id))
+      .catch((error) => console.error("Error fetching last product ID:", error));
+  }, []);
+
+  const fetchLastProductId = async () => {
+    try {
+      const response = await axios.get("/api/products");
+      const products = response.data.products;
+      const lastProduct = products[products.length - 1];
+      return lastProduct ? lastProduct.id + 1 : 1;
+    } catch (error) {
+      throw new Error("Error fetching last product ID");
+    }
+  };
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -11,6 +57,30 @@ const AddProductModal = ({ variant }: { variant: string }) => {
     setOpen(false);
   };
 
+  const handleSave = async () => {
+    try {
+      const newProductData = {
+        ...productData,
+        id: lastProductId || 0,
+      };
+      const response = await axios.post("/api/addProduct", { product: newProductData });
+
+      if (response.data.success) {
+        console.log("Product added successfully!");
+        addProduct(newProductData);
+        setOpen(false);
+      } else {
+        console.error("Error adding the product.");
+      }
+    } catch (error) {
+      console.error("Error adding the product.", error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    setProductData((prevData) => ({ ...prevData, [id]: value }));
+  };
 
   return (
     <div>
@@ -18,36 +88,67 @@ const AddProductModal = ({ variant }: { variant: string }) => {
         ADD PRODUCT
       </Button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Add Physical Product</DialogTitle>
+        <DialogTitle id="form-dialog-title">Add Product</DialogTitle>
         <DialogContent>
-          {/* Subcategory Name */}
-          <TextField
-            autoFocus
-            margin="dense"
-            id="subcategoryName"
-            label="Subcategory Name"
-            fullWidth
-          />
-          {/* Upload Image */}
-          <TextField
-            margin="dense"
-            id="uploadImage"
-            type="file"
-            fullWidth
-          />
-
+        <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Name"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="photo"
+                        label="Photo"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="price"
+                        label="Price"
+                        type="number"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="status"
+                        label="Status"
+                        select
+                        fullWidth
+                        value={productData?.status || ""}
+                        onChange={handleInputChange}
+                    >
+                        <MenuItem value="in stock">In Stock</MenuItem>
+                        <MenuItem value="low quantity">Low Quantity</MenuItem>
+                        <MenuItem value="not available">Not Available</MenuItem>
+                        <MenuItem value="deleted">Deleted</MenuItem>
+                    </TextField>
+                    <TextField
+                        margin="dense"
+                        id="description"
+                        label="Description"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="category"
+                        label="Category"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            Save
-          </Button>
-          <Button onClick={handleClose} color="primary">
             Close
           </Button>
-          {/* {variant === "pencil" && showAdditionalFields && (
-            <Button onClick={handleHideAdditionalFields} color="primary">
-            </Button>
-          )} */}
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
---
+++ pages/api/addProduct.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+import path from "path";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Product = {
+  id: number;
+  name: string;
+  photo: string;
+  price: number;
+  status: string;
+  description: string;
+  category: string;
+  vendorId: number;
+};
+
+const productsPath = path.join(process.cwd(), "public", "products.json");
+
+function getNextProductId(existingProducts: Product[]): number {
+  const lastProductId = existingProducts.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+  return lastProductId + 1;
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    console.log("Received POST request:", req.body);
+
+    const { name, photo, price, status, description, category }: Product = req.body.product;
+    const newProduct: Product = {
+      id: 0,
+      name,
+      photo,
+      price,
+      status,
+      description,
+      category,
+      vendorId: 1,
+    };
+
+    try {
+      console.log("Reading products file...");
+      if (!fs.existsSync(productsPath)) {
+        fs.writeFileSync(productsPath, JSON.stringify({ products: [] }), "utf8");
+      }
+      const productsData = fs.readFileSync(productsPath, "utf8");
+      const productsObj = JSON.parse(productsData);
+      const existingProducts: Product[] = productsObj.products;
+      newProduct.id = getNextProductId(existingProducts);
+
+      const updatedProducts: Product[] = [...existingProducts, newProduct];
+      productsObj.products = updatedProducts;
+
+      console.log("Writing updated products to file...");
+      fs.writeFileSync(productsPath, JSON.stringify(productsObj, null, 2));
+
+      res.status(200).json({ success: true, message: "Product added successfully!" });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ success: false, message: "Error adding the product." });
+    }
+  } else {
+    res.status(405).json({ success: false, message: "Method not allowed." });
+  }
+}
